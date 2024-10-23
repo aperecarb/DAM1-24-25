950a5409124aa8042a4e8d94d81125f395efbb80
aguadodev <aguado@edu.xunta.es>
2024-10-23 09:14:33 +0200

    Proyectos - UsuariosBD

---
+++ src/proyectos/usuarios/Conexion.java
@@ -0,0 +1,8 @@
+package proyectos.usuarios;
+class Conexion {
+    static final String HOST = "7qq-d.h.filess.io";
+    static final String DATABASE = "dam1_notecornam";
+    static final String USER = "dam1_notecornam";
+    static final String PASSWORD = "5d909372ed1f2f177f80c7ddae92ba8981f43fff";
+    static final String PORT = "3305";
+}
