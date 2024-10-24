2c31144c3655d34d2ef4e34a487b6c8516ce258b
aguadodev <aguado@edu.xunta.es>
2024-10-24 09:50:25 +0200

    Ejercicios Resueltos Condicionales

--- src/ud1/ejercicios/EP0126.java
+++ src/ud1/ejercicios/EP0126.java
@@ -5,14 +5,15 @@ public class EP0126 {
         // Declaración de variables y constantes
         double numero, raiz;
         // En este ejercicio usamos la clase Scanner sin importarla previamente.
-        java.util.Scanner entrada;
-        
-        entrada = new java.util.Scanner(System.in);
+        java.util.Scanner sc;
+
+        sc = new java.util.Scanner(System.in);
 
         // Entrada de datos
         System.out.print("Introduce un número real: ");
-        numero = entrada.nextDouble();
-
+        numero = sc.nextDouble();
+        sc.close();
+        
         // Proceso
         raiz = Math.sqrt(numero);
 
--- src/ud2/ejercicioscondicionales/Bisiesto.java
+++ src/ud2/ejercicioscondicionales/Bisiesto.java
@@ -1,4 +1,4 @@
-package ud2.ejercicios;
+package ud2.ejercicioscondicionales;
 
 import java.util.Scanner;
 
@@ -21,21 +21,14 @@ public class Bisiesto {
         Scanner sc = new Scanner(System.in);
         System.out.println("Año: ");
         anho = sc.nextInt();
+        sc.close();
 
+        // CONDICIONES A EVALUAR PARA SER BISIESTO
         boolean div4 = anho % 4 == 0;
         boolean div100 = anho % 100 == 0;
         boolean div400 = anho % 400 == 0;
 
-        esBisiesto = div400 || div4 && !div100;
-
-        /*
-        if (div400 || div4 && !div100) {
-            esBisiesto = true;
-        } else {
-            esBisiesto = false;
-        }
- */
-   /*
+        /* SOLUCIÓN CON IFs ANIDADOS
         if (div4) {
             if (div100) {
                 if (div400) {
@@ -49,7 +42,32 @@ public class Bisiesto {
         } else {
             esBisiesto = false;
         }
- */     
+        */     
+
+        /* OTRA SOLUCIÓN CON IFs ANIDADOS
+        esBisiesto = false;
+        if (anho % 4 == 0) {
+            if (anho % 100 != 0) {
+                esBisiesto = true;
+            } else {
+                if (anho % 400 == 0) {
+                    esBisiesto = true;
+                }
+            }
+        }
+        */
+        
+        /* SOLUCIÓN CON IF-ELSE Y EXPRESIÓN LÓGICA COMPLEJA
+        if (div400 || div4 && !div100) {
+            esBisiesto = true;
+        } else {
+            esBisiesto = false;
+        }
+        */
+        
+        // SOLUCIÓN EN UNA ASIGNACIÓN
+        esBisiesto = div400 || div4 && !div100;
+
         if (esBisiesto) {
             System.out.println("El año " + anho + " es bisiesto.");
         } else {
