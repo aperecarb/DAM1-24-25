ac9e7a27eb60145618f919bd577b2a3c0eee2420
aguadodev <aguado@edu.xunta.es>
2024-10-24 12:45:23 +0200

    Ejercicios UD2

--- src/ud1/naoexamen/CompraConjunta.java
+++ src/ud1/naoexamen/CompraConjunta.java
@@ -1,47 +1,51 @@
+//Saúl Fernández Salgado
+
 package ud1.naoexamen;
 
 import java.util.Scanner;
 
-/**
- * Tres amig@s deciden juntar el dinero en efectivo que llevan encima para
- * comprar un artículo para compartir (por ejemplo, una pelota, un libro, un
- * disco, etc.). Haz un programa que permita introducir el precio del artículo
- * (en euros) y el dinero que aporta cada amig@ y que informe si el dinero
- * aportado es suficiente o no para adquirir el artítulo.
- * Si el dinero no es suficiente el programa informará de cuánto falta, y si
- * sobra, se indicará el importe del dinero sobrante que guardará como bote para
- * futuras compras conjuntas.
- */
 public class CompraConjunta {
-    public static void main(String[] args) {
-        // Variables de entrada
-        double precio, dineroAmigo1, dineroAmigo2, dineroAmigo3;
-        // Variables de salida
-        boolean dineroSuficiente;
-        double dineroBote, dineroFaltante;
 
-        // ENTRADA
+    public static void main(String[] args) {
+        double precioTotalProducto;
+        double precio1, precio2, precio3;
+        double bote;
+        String respuesta;
+
+        // Pido datos por pantalla
+
         Scanner sc = new Scanner(System.in);
-        System.out.print("Precio del producto: ");
-        precio = sc.nextDouble();
-        System.out.print("Dinero aportado por cada uno de los 3 amigos: ");
-        dineroAmigo1 = sc.nextDouble();
-        dineroAmigo2 = sc.nextDouble();
-        dineroAmigo3 = sc.nextDouble();
+
+        System.out.println("Este programa pedirá el precio de un artículo por" +
+                " pantalla y la cantidad de dinero aportada por cada uno de los tres individuos" +
+                " que participan en el pago");
+        System.out.println();
+
+        System.out.println("Introduzca el precio del artículo a comprar:");
+        precioTotalProducto = sc.nextDouble();
+
+        System.out.println("Por favor, introduzca la cantidad que pagará el primer individuo:");
+        precio1 = sc.nextDouble();
+
+        System.out.println("Por favor, introduzca la cantidad que pagará el segundo individuo:");
+        precio2 = sc.nextDouble();
+
+        System.out.println("Por favor, introduzca la cantidad que pagará el tercer individuo:");
+        precio3 = sc.nextDouble();
+
         sc.close();
 
-        // PROCESO
-        double sumaDineroAmigos = dineroAmigo1 + dineroAmigo2 + dineroAmigo3;
+        // Realizo los calculos
 
-        dineroSuficiente = sumaDineroAmigos >= precio;
-        dineroBote = sumaDineroAmigos - precio;
-        dineroFaltante = precio - sumaDineroAmigos;
+        bote = precio1 + precio2 + precio3;
 
-        // SALIDA
-        String msjBote = String.format("Compramos el producto y quedan %.2f euros de bote", dineroBote);
-        String msjFalta = String.format("Faltan %.2f euros para comprar el producto.", dineroFaltante);
-        String mensaje = dineroSuficiente ? msjBote : msjFalta;
+        bote -= precioTotalProducto;
 
-        System.out.println(mensaje);
+        respuesta = bote == 0 ? "Se ha comprado el artículo con el precio justo" : 
+                    bote > 0  ? "Sobran para el bote " + bote + " euros" : 
+                                "Faltan " + -bote + " euros";
+                                
+        System.out.println(respuesta);
+
     }
 }
