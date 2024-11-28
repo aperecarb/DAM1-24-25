34e720867d04a35c1e25a3039f899a512a11bd50
aguadodev <aguado@edu.xunta.es>
2024-11-28 12:51:38 +0100

    Más POO

--- src/ud3/apuntes/CuentaCorriente.java
+++ src/ud3/apuntes/CuentaCorriente.java
@@ -2,27 +2,46 @@ package ud3.apuntes;
 
 public class CuentaCorriente {
     String dni;
-    public String nombre;
+    public String titular;
     private double saldo;
+    private Gestor gestor;
+    private static String banco;
 
 
-    public CuentaCorriente(String dni, String nombre, double saldo) {
+    public CuentaCorriente(String dni, String titular, double saldo) {
         this.dni = dni;
-        this.nombre = nombre;
+        this.titular = titular;
         this.saldo = saldo < 0 ? 0 : saldo;
     }
 
-    public CuentaCorriente(String dni, String nombre) {
-        this(dni, nombre, 0);
+    public CuentaCorriente(String dni, String titular) {
+        this(dni, titular, 0);
     }
-    
+
     public CuentaCorriente(String dni, double saldo) {
         this(dni, null, saldo);
     }
 
+    public static String getBanco() {
+        return banco;
+    }
+
+    public static void setBanco(String banco) {
+        CuentaCorriente.banco = banco;
+    }
+
+    
+
+    public Gestor getGestor() {
+        return gestor;
+    }
+
+    public void setGestor(Gestor gestor) {
+        this.gestor = gestor;
+    }
 
     public boolean sacarDinero(double importe) {
-        if (saldo >= importe)  {
+        if (saldo >= importe) {
             saldo -= importe;
             return true;
         } else {
@@ -34,10 +53,30 @@ public class CuentaCorriente {
         saldo += importe;
     }
 
-    public void mostrarInformacion(){
+    public void mostrarInformacion() {
         System.out.println("CUENTA CORRIENTE: ");
-        System.out.println("- Titular: " + nombre + " (DNI = " + dni + ")");
+        System.out.println("- Titular: " + titular + " (DNI = " + dni + ")");
         System.out.println("- Saldo: " + saldo + " euros");
-    }    
+    }
+
+
+
+    public static void main(String[] args) {
+        CuentaCorriente cc = new CuentaCorriente("55555555Z", "Pepe", 1000);
+        CuentaCorriente cc2 = new CuentaCorriente("33333333R", "María", 2000);
+
+        CuentaCorriente.setBanco("Abanca");
+
+        System.out.println(CuentaCorriente.getBanco());
+        cc.mostrarInformacion();
+        cc2.mostrarInformacion();
+
+        Gestor gestor = new Gestor("Gestoría Pepe", "986868686");
+        cc.gestor = gestor;
+        System.out.println(cc.gestor.nombre + "(" + cc.gestor.telefono + ")");
+        
+    }
+
+
 
 }
--- src/ud3/ejerciciosclases/MarcaPagina.java
+++ src/ud3/ejerciciosclases/MarcaPagina.java
@@ -2,18 +2,51 @@ package ud3.ejerciciosclases;
 
 public class MarcaPagina {
     int pagina;
-    String libro;
+    //String libro;
     
     public void incrementarPagina() {
         pagina++;
     }
 
-    public int getPagina() {
-        return this.pagina;
-    }
 
     public void reiniciarLibro() {
         pagina = 0;
     }
+
+    // GETTERS Y SETTERS
+    public int getPagina() {
+        return pagina;
+    }
+
+    public void setPagina(int pagina) {
+        if (pagina >= 0)
+            this.pagina = pagina;
+    }
+
+
+
+    public static void main(String[] args) {
+        // Crea  marcapáginas
+        MarcaPagina mp = new MarcaPagina();
+        MarcaPagina mp2 = new MarcaPagina();
+
+        // Imprime la página actual (página inicial)
+        System.out.println(mp.getPagina());
+
+        // Incrementamos la página
+        mp.incrementarPagina();
+        mp.incrementarPagina();
+
+        System.out.println(mp.getPagina());
+
+        // Reiniciar libro
+        mp.reiniciarLibro();
+
+        System.out.println(mp.getPagina());
+
+    }
+
+
+
     
 }
