93c22ad0f8e79a470c8ed4cedbed5ec85590fbe2
aguadodev <aguado@edu.xunta.es>
2025-01-23 12:10:06 +0100

    Strings

--- src/ud4/stringsapuntes/E0601.java
+++ src/ud4/stringsapuntes/E0601.java
@@ -3,7 +3,6 @@ package ud4.stringsapuntes;
 public class E0601 {
     public static void main(String[] args) {
         for (int i = 0; i < 512; i++) // máximo = 65536
-            System.out.println(i + " -> " + (char) i );
-        
+            System.out.println(i + " -> " + (char) i );        
     }
 }
