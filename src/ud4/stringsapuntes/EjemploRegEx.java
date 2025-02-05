af02e7f3490338805a083e2c0ddac1f376320fce
aguadodev <aguado@edu.xunta.es>
2025-02-05 14:30:40 +0100

    Matrículas

---
+++ src/ud4/stringsapuntes/EjemploRegEx.java
@@ -0,0 +1,23 @@
+package ud4.stringsapuntes;
+
+import java.util.*;
+
+class EjemploRegEx {
+
+    public static void main(String[ ] args) {
+
+        String respuesta;
+        boolean siguePatron;
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.print("Escribe una matrícula: ");
+            respuesta = sc.nextLine();
+            siguePatron = respuesta.matches("[0-9]{4}[BCDFGHJKLMNPRSTVWXYZ]{3}");
+            if(!siguePatron)
+                System.out.println("La expresion no encaja con el patrón: --[0-9]{3}[A-Z]+");
+        } while(!siguePatron);
+
+        System.out.println("Expresión correcta!");
+    }
+}
+
