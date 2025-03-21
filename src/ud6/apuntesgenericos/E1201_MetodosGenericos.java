5aea19742773188f1b2700e497c28a57bc9fe8c3
aguadodev <aguado@edu.xunta.es>
2025-03-21 13:44:58 +0100

    Métodos Genéricos

--- src/ud6/apuntesgenericos/E1201_MetodosGenericos.java
+++ src/ud6/apuntesgenericos/E1201_MetodosGenericos.java
@@ -1,5 +1,7 @@
 package ud6.apuntesgenericos;
 
+import ud3.apuntes.Persona;
+
 public class E1201_MetodosGenericos {
     static <U> int numeroDeNulos(U[] tabla) {
         int cont = 0;
@@ -7,5 +9,13 @@ public class E1201_MetodosGenericos {
             if (e == null) 
                 cont++;
         return cont;
+    }
+
+    public static void main(String[] args) {
+        Integer[] numeros = {3, 5, 8, null, 2, null};
+        Persona[] personas = {new Persona("Pepe"), null, new Persona("María")};
+
+        System.out.println(numeroDeNulos(numeros));
+        System.out.println(numeroDeNulos(personas));
     }
 }
