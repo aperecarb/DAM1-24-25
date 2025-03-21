c40fdc94388779179dff74850af66a851bccb00c
aguadodev <aguado@edu.xunta.es>
2025-03-21 14:01:12 +0100

    Genericos

--- src/ud6/apuntesgenericos/E1201_MetodosGenericos.java
+++ src/ud6/apuntesgenericos/E1201_MetodosGenericos.java
@@ -1,6 +1,8 @@
 package ud6.apuntesgenericos;
 
-import ud3.apuntes.Persona;
+import java.util.Arrays;
+
+import ud5.apuntesherencia.Persona;
 
 public class E1201_MetodosGenericos {
     static <U> int numeroDeNulos(U[] tabla) {
@@ -11,11 +13,38 @@ public class E1201_MetodosGenericos {
         return cont;
     }
 
+    public static <U> U[] add(U e, U[] t) {
+        return null;
+    }
+
+    public static <U> boolean buscar(U e, U[] t) {
+        return true;
+    }
+
+    public static <U> U[] concatenar(U[] t1, U[] t2) {
+        return null;
+    }
+
+
     public static void main(String[] args) {
         Integer[] numeros = {3, 5, 8, null, 2, null};
         Persona[] personas = {new Persona("Pepe"), null, new Persona("María")};
 
         System.out.println(numeroDeNulos(numeros));
         System.out.println(numeroDeNulos(personas));
+
+        numeros = add(7, numeros);
+        System.out.println(Arrays.toString(numeros));
+        personas = add(new Persona("Juancho"), personas);
+        System.out.println(Arrays.toString(personas));
+
+        System.out.println("Hay un 8 en números? " + buscar(8, numeros));
+        System.out.println("Hay un 6 en números? " + buscar(6, numeros));
+
+        System.out.println("Está Pepe? " + buscar(new Persona("Pepe"), personas));
+
+        Integer[] numeros2 = {23, 45, 78, 23};
+        Integer[] masNumeros = concatenar(numeros, numeros2);
+        System.out.println(Arrays.toString(masNumeros));
     }
 }
