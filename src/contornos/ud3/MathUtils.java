cbd7a2177281182c4a2eed7d8ae0e78af311f7c0
aguadodev <aguado@edu.xunta.es>
2025-03-25 11:25:46 +0100

    Contornos Probas e Erros

--- src/contornos/ud3/MathUtils.java
+++ src/contornos/ud3/MathUtils.java
@@ -6,7 +6,7 @@ public class MathUtils {
             throw new IllegalArgumentException("O número debe ser positivo");
         }
         int resultado = 1;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i--) { // Erro: debería ser i++
             resultado *= i;
         }
         return resultado;
--- src/contornos/ud3/PasswordValidator.java
+++ src/contornos/ud3/PasswordValidator.java
@@ -2,20 +2,20 @@ package contornos.ud3;
 
 public class PasswordValidator {
     public static boolean isValid(String password) {
-        if (password.length() < 8) {
+        if (password.length() <= 8) {
             return false;
         }
         boolean hasUpperCase = false;
         boolean hasDigit = false;
         for (char c : password.toCharArray()) {
-            if (Character.isUpperCase(c)) {
+            if (Character.isLowerCase(c)) {
                 hasUpperCase = true;
             }
             if (Character.isDigit(c)) {
-                hasDigit = true;
+                hasDigit = false;
             }
         }
-        return hasUpperCase && hasDigit;
+        return hasUpperCase || hasDigit;
     }
 }
 
