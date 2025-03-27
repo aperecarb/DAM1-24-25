1847ea65e150057cef29bbab6c8d5bf342298a8b
aguadodev <aguado@edu.xunta.es>
2025-03-27 12:44:39 +0100

    ContenedorLista

--- src/ud6/apuntesgenericos/ContenedorLista.java
+++ src/ud6/apuntesgenericos/ContenedorLista.java
@@ -7,29 +7,50 @@ import ud5.apuntesherencia.Persona;
 public class ContenedorLista<T> {
     T[] objetos;
 
-    
     public ContenedorLista(T[] t) {
         objetos = t;
     }
 
-    void insertarAlPrincipio(T nuevo){
-        objetos = Arrays.copyOf(objetos,objetos.length + 1); 
-        System.arraycopy(objetos, 0, objetos, 1, objetos.length);
+    void insertarAlPrincipio(T nuevo) {
+        objetos = Arrays.copyOf(objetos, objetos.length + 1);
+        for (int i = 0; i < objetos.length - 1; i++) {
+            objetos[i + 1] = objetos[i];
+        }
         objetos[0] = nuevo;
     }
-    void insertarAlFinal(T nuevo){
-        objetos = Arrays.copyOf(objetos,objetos.length + 1);
+
+    void insertarAlFinal(T nuevo) {
+        objetos = Arrays.copyOf(objetos, objetos.length + 1);
         objetos[objetos.length - 1] = nuevo;
     }
-    T extraerDelPrincipio(){
-        return null;
-    }
-    T extraerDelFinal(){
-        return null;
-    }
-    void ordenar(){
 
+    T extraerDelPrincipio() {
+        T valor = null;
+        if (objetos.length > 0) {
+            valor = objetos[0];
+            for (int i = 0; i < objetos.length; i++)
+                objetos[i] = objetos[i+1];
+            objetos = Arrays.copyOf(objetos, objetos.length - 1);
+        } 
+        return valor;
     }
+
+    T extraerDelFinal() {
+        T valor = null;
+        if (objetos.length > 0) {
+            valor = objetos[objetos.length - 1];
+            objetos = Arrays.copyOf(objetos, objetos.length - 1);            
+        }
+        return valor;
+    }
+
+    void ordenar() {
+        /* Generará una excepción si T no es Comparable "T cannot be cast to class java.lang.Comparable"
+         * Se puede arreglar indicando que T debe implementar Comparable "T extends Comparable"
+         */
+        Arrays.sort(objetos);  
+    }
+
     @Override
     public String toString() {
         return Arrays.toString(objetos);
@@ -48,7 +69,9 @@ public class ContenedorLista<T> {
         listaP.insertarAlFinal(new Persona("Lola"));
         System.out.println(listaP);
 
+        listaP.ordenar();
+
+        System.out.println(listaP);
     }
-    
 
 }
