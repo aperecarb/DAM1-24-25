318438c2c711da1754ad7fbca22bf07b88e5fa9a
aguadodev <aguado@edu.xunta.es>
2025-04-04 14:29:54 +0200

    Mapas

--- src/ud6/apuntescolecciones/Sorteo.java
+++ src/ud6/apuntescolecciones/Sorteo.java
@@ -1,21 +1,51 @@
 package ud6.apuntescolecciones;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 import ud5.rol.Personaje;
 import ud5.rol.Personaje.Raza;
 
 public class Sorteo<T> {
+
+    Set<T> elementos;
+
+    public Sorteo() {
+        elementos = new HashSet<>();
+    }
+
     boolean add(T elemento) {
-        return true;
+        return elementos.add(elemento);
     }
 
     Set<T> premiados(int numPremiados) {
-        return null;
+
+        if (numPremiados <= 0)
+            return null;
+
+        if (numPremiados > elementos.size())
+            numPremiados = elementos.size();
+
+        Set<T> premiados = new LinkedHashSet<>();
+
+        List<T> lista = new ArrayList<>(elementos);
+        Collections.shuffle(lista);
+
+        for (int i = 0; i < numPremiados; i++) {
+            premiados.add(lista.get(i));
+        }
+
+        return premiados;
     }
 
     public static void main(String[] args) {
-        // Crea un sorteo entre 4 personajes para seleccionar los 2 que se enfrentarán en combate singular
+        // Crea un sorteo entre 4 personajes para seleccionar los 2 que se enfrentarán
+        // en combate singular
         Sorteo<Personaje> sorteo = new Sorteo<>();
         sorteo.add(new Personaje("Aragorn", Raza.HUMANO, 90, 90, 90, 10, 16252, 125));
         sorteo.add(new Personaje("Mara", Raza.ELFO, 100, 100, 100));
